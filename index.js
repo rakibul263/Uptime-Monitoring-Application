dd0742a83b76bf54fc0f5595404468a9d468e5ec
rakibul263 <rakibulhasanshuvo206@gmail.com>
2026-05-15 11:33:03 +0600

    create a server

--- index.js
+++ index.js
@@ -4,3 +4,30 @@
  * Author      : MD Rakibul Hasan
  * Date        : 2026-05-15
  */
+
+// dependencies
+const http = require("http");
+
+// app object - module scaffoholding
+const app = {};
+
+// configuration
+app.config = {
+  PORT: 3000,
+};
+
+app.createServer = () => {
+  const server = http.createServer(app.handleReqRes);
+  server.listen(app.config.PORT, () => {
+    console.log(`The server is running at ${app.config.PORT}`);
+  });
+};
+
+// handle request and response
+app.handleReqRes = (req, res) => {
+  // response handle
+  res.end("Hello World");
+};
+
+// start server
+app.createServer();
