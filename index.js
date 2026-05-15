4fd7082a1b685cd0dacaa2b5f8a67d01c96cbbfa
rakibul263 <rakibulhasanshuvo206@gmail.com>
2026-05-15 10:18:26 +0600

    add Title description author and date

---
+++ index.js
@@ -0,0 +1,6 @@
+/*
+ * Title       : Uptime Monitoring Application (Raw Node.js)
+ * Description : A RESTful API server built with pure Node.js (no framework) for monitoring website uptime and sending SMS alerts when status changes (UP/DOWN).
+ * Author      : MD Rakibul Hasan
+ * Date        : 2026-05-15
+ */
